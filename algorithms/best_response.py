1ad1a9bb40d04726d860047b4062f8309f30f01c
meerajagota <meerajagota16@gmail.com>
2023-05-29 12:58:39 -0700

    colab

--- algorithms/best_response.py
+++ algorithms/best_response.py
@@ -16,13 +16,14 @@ import torch.nn.functional as F
 import torch.optim as optim
 #sys.path.append(r"C:\Users\Administrator\Desktop\multiagent-catch-flag")
 sys.path.append(r"../")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def pruneobs(obs):
     newarray = np.zeros(shape=(7, 7, 4), dtype=np.uint8)
     for a in range(7):
         for b in range(7):
-            #print(len(obs[a][b]))
+
             newarray[a][b] = np.concatenate((obs[a][b][0:2],obs[a][b][4:6]), axis =0)
     return newarray
 
@@ -39,6 +40,7 @@ class QNetwork(nn.Module):
             nn.Linear(32*3*3, 64),
             nn.Linear(64, num_actions),
         )
+        self.conv.to(device)
 
 
 
@@ -49,8 +51,7 @@ class QNetwork(nn.Module):
     def forward(self, x):
 
         x = self.conv(x)
-        q_values = x
-        return q_values
+        return x
 # Define the DQN Agent
 class DQNAgent:
     def __init__(self, num_actions, lr, gamma, epsilon_max, epsilon_min, epsilon_decay, buffer_capacity, batch_size):
@@ -73,7 +74,7 @@ class DQNAgent:
         if random.random() < self.epsilon:
             return random.randint(0, self.num_actions - 1)
         else:
-            state = torch.from_numpy(state).unsqueeze(0).float()
+            state = torch.cuda.FloatTensor(np.array(state)).unsqueeze(0).to(device)
             q_values = self.q_network(state)
             return torch.argmax(q_values).item()
         
@@ -88,38 +89,46 @@ class DQNAgent:
         batch = random.sample(self.buffer, self.batch_size)
         states, actions, rewards, next_states, dones = zip(*batch)
 
-        states = torch.FloatTensor(states)
-        actions = torch.LongTensor(actions)
-        rewards = torch.FloatTensor(rewards)
-        next_states = torch.FloatTensor(next_states)
-        dones = torch.BoolTensor(dones)
-
+        # Convert numpy arrays to PyTorch tensors
+        states = torch.cuda.FloatTensor(np.array(states)).to(device)
+        actions = torch.LongTensor(np.array(actions)).to(device)
+        rewards = torch.FloatTensor(np.array(rewards)).to(device)
+        next_states = torch.cuda.FloatTensor(np.array(next_states)).to(device)
+        dones = torch.BoolTensor(np.array(dones)).to(device)
         q_values = self.q_network(states)
-        q_values = q_values.gather(1, actions.unsqueeze(1)).squeeze(1)
-
-        next_q_values = self.target_network(next_states).detach()
-        max_next_q_values = next_q_values.max(1)[0]
-        expected_q_values = rewards + self.gamma * max_next_q_values * ~dones
+        q_values = q_values.gather(1, actions.unsqueeze(1)).squeeze(1).to(device)
+        next_q_values = self.target_network(next_states)
+        max_next_q_values = torch.max(next_q_values, dim=1)[0]
+        expected_q_values = rewards + self.gamma * max_next_q_values * (~dones)   
 
         loss = self.loss(q_values, expected_q_values)
+
+        # q_network_params_before = [param.data.clone() for param in self.q_network.parameters()]
 
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
+        # # Check if the parameter tensors have changed
+        # q_network_params_after = [param.data for param in self.q_network.parameters()]
+        # params_changed = any(not torch.allclose(param_before, param_after) for param_before, param_after in zip(q_network_params_before, q_network_params_after))
+
+        # if params_changed:
+        #     print("DQN parameters have changed.")
+        # else:
+        #     print("DQN parameters have not changed.")
+        
+
 
 # Define the main training loop
 def main():
-
     register(
             id='multigrid-collect-v0',
             entry_point='gym_multigrid.envs:CollectGame4HEnv10x10N2',
         )
     env = gym.make('multigrid-collect-v0')
-
     # state_shape = env.observation_space.shape
     # state_shape = [1,7,7,4]
-
     num_actions = env.action_space.n
     lr = 0.1
     gamma = 0.99
@@ -128,60 +137,53 @@ def main():
     epsilon_decay = 500
     buffer_capacity = 10000
     batch_size = 32
-
     adversary = DQNAgent(num_actions, lr, gamma, epsilon_max, epsilon_min, epsilon_decay, buffer_capacity, batch_size)
-
     num_episodes = 1000
-    for episode in range(num_episodes):
-    
+    eps_returns = []
+    eps_step =[]
+    for episode in range(num_episodes):  
         states = env.reset()
-        print(env.remaining_ball)
-
+        #print(env.remaining_ball)
         done = False
-        total_reward = 0
-
+        total_reward = 0 
+        steps = 0      
         while not done:
             # env.render(mode='human', highlight=True)
-
             newobs = [pruneobs(agent) for agent in states] ##use newobs
-            newobs[0] = np.transpose(newobs[0], (2, 0, 1))
-
+            newobs[0] = np.transpose(newobs[0], (2, 0, 1))           
             actions = []
-
-
+            steps += 1
             # Select actions for the adversary agent only
             actions.append(adversary.select_action(newobs[0]))
 
             for _ in range(1, len(newobs)):
                 actions.append(env.action_space.sample())
 
-            next_states, rewards, done, _ = env.step(actions)
-
+            states, rewards, done, _ = env.step(actions)
+            newobs1 = [pruneobs(agent) for agent in states] ##use newobs
+            newobs1[0] = np.transpose(newobs1[0], (2, 0, 1))
             # Buffer experiences and update only for the adversary agent
-            if env.remaining_ball <= 0:
-                adversary.buffer.append((newobs[0], actions[0], rewards[0], next_states[0], done))
-                adversary.update()
-                done = True
-
-            states = next_states
             total_reward += rewards[0] 
-
+            adversary.buffer.append((newobs[0], actions[0], rewards[0], newobs1[0], done))       
+        adversary.update()      
         # Update the target network every few episodes
         if episode % 5 == 0:
-
             adversary.target_network.load_state_dict(adversary.q_network.state_dict())
+        if episode % 100 == 1:
+            torch.save(adversary.target_network.state_dict(), f'adversary{episode}.pt')
 
         time.sleep(5)
-
-
+        eps_returns.append(total_reward)
+        eps_step.append(steps)
         print(f"Episode: {episode+1}, Total Reward: {total_reward}")
+        if episode%20 == 10:
+            
+            print(f'current return {sum(eps_returns[-10:])/10}')
+            print(f'current steps {sum(eps_step[-10:])/10}')
+
 
         # Update DQN here
         adversary.update_epsilon(episode)
-
-        
-
     env.close()
-
 # Start training
 main()
