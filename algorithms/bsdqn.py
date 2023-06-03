c8bdac75baf683f958ae661bb8b7f3bda724ccfd
duduyiq2001 <86567562+duduyiq2001@users.noreply.github.com>
2023-06-03 12:46:58 -0700

    frkfrr

--- algorithms/bsdqn.py
+++ algorithms/bsdqn.py
@@ -35,8 +35,46 @@ register(
 multienv = gym.make('multigrid-collect-v0')
 env = SingleAgentWrapper(multienv)
 check_env(env)
-#model = DQN(policy="MlpPolicy", env=env, verbose=1,tensorboard_log= "./")
-model = DQN.load("bsdpnfulladv", env=env)
-model.policy_kwargs = dict(eps_start=0.05, eps_end=0.03)
-model.learn(total_timesteps=600000,progress_bar=True,log_interval=5)
+model = DQN(policy="MlpPolicy", env=env, verbose=1,tensorboard_log= "./")
+#model = DQN.load("bsdpnfulladv",env=env,exploration_initial_eps=0.05, exploration_final_eps=0.03)
+model.learn(total_timesteps=800000,progress_bar=True,log_interval=5)
+import imageio
+def record_video(out_directory,env,model, fps=30):
+    """
+    Generate a replay video of the agent
+    :param env
+    :param Qtable: Qtable of our agent
+    :param out_directory
+    :param fps: how many frame per seconds (with taxi-v3 and frozenlake-v1 we
+    """
+    images = []
+    state = env.reset()
+    state = np.array(state).astype(np.float32).flatten()
+    done = False
+    img = env.render(mode='rgb_array')
+    images.append(img)
+    counter = 0
+    while not done:
+        # Take the action (index) that have the maximum expected future reward g
+        #action, _ = policy.act(state)
+        actions = []
+        print(model.predict(state)[0])
+        #print(multienv)
+        actions.append(model.predict(state)[0])
+        actions.append(0)
+        actions.append(0)
+        #print(counter)
+        counter +=1
+        state, reward, done, info = env.step(actions) # We directly put next_stat
+        state = np.array(state).astype(np.float32).flatten()
+        img = env.render(mode='rgb_array')
+        env.render(mode= 'human')
+        images.append(img)
+        imageio.mimsave(out_directory, [np.array(img) for i, img in enumerate(images)])
+        if counter >= 200:
+            break
+
+record_video('./replay2.mp4',env=multienv,model=model)
+
+
 model.save("bsdpnfulladv1")
